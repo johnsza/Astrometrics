e13757797bf50a80555f2cf963989bc976c1bcca
johnsza <johnsza@hawaii.edu>
2025-05-11 11:04:32 -1000

    Added Navigation pane to each page of the website. Some styling completed on index.

--- main.js
+++ main.js
@@ -19,10 +19,11 @@ function generateQuote() {
 }
 
 function openNav() {
+    console.log("openNav function called");
     document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-  } 
+} 
